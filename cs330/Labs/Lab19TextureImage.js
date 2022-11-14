861b2d534fbc7d868813210b89605a681171bdec
Pawjwp <pawjwp@users.noreply.github.com>
2022-11-14 10:06:23 -0500

    CR Tweaks, Lab 18 and 19

--- cs330/Labs/Lab19TextureImage.js
+++ cs330/Labs/Lab19TextureImage.js
@@ -2,12 +2,12 @@
 var gl;
 var points;
 // isoceles triangles to form a tetrahedron
-    points=[
-    vec4(  0.00 ,  0.50 ,  0.00 , 1.0 ),
-    vec4( -0.50 , -0.50 ,  0.50 , 1.0 ),
-    vec4(  0.50 , -0.50 ,  0.50 , 1.0 ),
-    vec4(  0.50 , -0.50 , -0.50 , 1.0 )
-    ];
+points = [
+	vec4(0.00, 0.50, 0.00, 1.0),
+	vec4(-0.50, -0.50, 0.50, 1.0),
+	vec4(0.50, -0.50, 0.50, 1.0),
+	vec4(0.50, -0.50, -0.50, 1.0)
+];
 
 var flag = true;
 
@@ -38,47 +38,45 @@ var texCoordsArray = [];
 
 // perpendicular texture w/isoceles triangle
 var texCoord = [
-    vec2(0.5, 0),
-    vec2(0  , 1),
-    vec2(1  , 1)
+	vec2(0.5, 0),
+	vec2(0, 1),
+	vec2(1, 1)
 ];
 
 var positionsArray = [];
 var colorsArray = [];
 var vertexColors = [
-    vec4(0.0, 0.0, 1.0, 1.0),  // blue
-    vec4(1.0, 0.0, 0.0, 1.0),  // red
-    vec4(1.0, 1.0, 0.0, 1.0),  // yellow
-    vec4(0.0, 1.0, 0.0, 1.0)  // green
+	vec4(0.0, 0.0, 1.0, 1.0), // blue
+	vec4(1.0, 0.0, 0.0, 1.0), // red
+	vec4(1.0, 1.0, 0.0, 1.0), // yellow
+	vec4(0.0, 1.0, 0.0, 1.0) // green
 ];
 
 window.onload = init;
 
-function configureTexture( image ) {
+function configureTexture(image) {
 
 }
 
-function triangle (a,b,c,triNum)
-{
-   positionsArray.push(points[a]);
-   colorsArray.push(vertexColors[triNum]);
-   texCoordsArray.push(texCoord[0]);
+function triangle(a, b, c, triNum) {
+	positionsArray.push(points[a]);
+	colorsArray.push(vertexColors[triNum]);
+	texCoordsArray.push(texCoord[0]);
 
-   positionsArray.push(points[b]);
-   colorsArray.push(vertexColors[triNum]);
-   texCoordsArray.push(texCoord[1]);
+	positionsArray.push(points[b]);
+	colorsArray.push(vertexColors[triNum]);
+	texCoordsArray.push(texCoord[1]);
 
-   positionsArray.push(points[c]);
-   colorsArray.push(vertexColors[triNum]);
-   texCoordsArray.push(texCoord[2]);
+	positionsArray.push(points[c]);
+	colorsArray.push(vertexColors[triNum]);
+	texCoordsArray.push(texCoord[2]);
 }
 
-function colorTetra()
-{
-    triangle(0,1,2 ,0);
-    triangle(0,2,3 ,1);
-    triangle(0,3,1 ,2);
-    triangle(1,3,2 ,3);
+function colorTetra() {
+	triangle(0, 1, 2, 0);
+	triangle(0, 2, 3, 1);
+	triangle(0, 3, 1, 2);
+	triangle(1, 3, 2, 3);
 }
 
 var xAxis = 0;
@@ -90,71 +88,83 @@ var theta = vec3(45.0, 45.0, 45.0);
 
 var thetaLoc;
 
-function init()
-{
-    var canvas = document.getElementById( "gl-canvas" );
+function init() {
+	var canvas = document.getElementById("gl-canvas");
 
-    gl = canvas.getContext('webgl2');
-    if ( !gl ) { alert( "WebGL isn't available" ); }
+	gl = canvas.getContext('webgl2');
+	if (!gl) {
+		alert("WebGL isn't available");
+	}
 
-    colorTetra();
-    
-    //
-    //  Configure WebGL
-    //
-    gl.viewport( 0, 0, canvas.width, canvas.height );
-    gl.clearColor( 1.0, 1.0, 1.0, 1.0 );
-    gl.enable(gl.DEPTH_TEST);
+	colorTetra();
 
-    //  Load shaders and initialize attribute buffers
+	//
+	//  Configure WebGL
+	//
+	gl.viewport(0, 0, canvas.width, canvas.height);
+	gl.clearColor(1.0, 1.0, 1.0, 1.0);
+	gl.enable(gl.DEPTH_TEST);
 
-    program = initShaders( gl, "vertex-shader", "fragment-shader" );
-    gl.useProgram( program );
+	//  Load shaders and initialize attribute buffers
 
-    // Load the data into the GPU
+	program = initShaders(gl, "vertex-shader", "fragment-shader");
+	gl.useProgram(program);
 
-    // color buffer
-    var cBuffer = gl.createBuffer();
-    gl.bindBuffer(gl.ARRAY_BUFFER, cBuffer);
-    gl.bufferData(gl.ARRAY_BUFFER, flatten(colorsArray), gl.STATIC_DRAW);
-    var colorLoc =gl.getAttribLocation(program, "aColor");
-    gl.vertexAttribPointer(colorLoc, 4, gl.FLOAT, false, 0, 0);
-    gl.enableVertexAttribArray(colorLoc);
+	// Load the data into the GPU
 
-    // positions buffer
-    var vBuffer = gl.createBuffer();
-    gl.bindBuffer(gl.ARRAY_BUFFER, vBuffer);
-    gl.bufferData(gl.ARRAY_BUFFER, flatten(positionsArray), gl.STATIC_DRAW);
-    var positionLoc =gl.getAttribLocation( program, "aPosition");
-    gl.vertexAttribPointer(positionLoc, 4, gl.FLOAT, false, 0, 0);
-    gl.enableVertexAttribArray(positionLoc);
+	// color buffer
+	var cBuffer = gl.createBuffer();
+	gl.bindBuffer(gl.ARRAY_BUFFER, cBuffer);
+	gl.bufferData(gl.ARRAY_BUFFER, flatten(colorsArray), gl.STATIC_DRAW);
+	var colorLoc = gl.getAttribLocation(program, "aColor");
+	gl.vertexAttribPointer(colorLoc, 4, gl.FLOAT, false, 0, 0);
+	gl.enableVertexAttribArray(colorLoc);
 
-    // texture buffer
-    var tBuffer = gl.createBuffer();
-    gl.bindBuffer(gl.ARRAY_BUFFER, tBuffer);
-    gl.bufferData(gl.ARRAY_BUFFER, flatten(texCoordsArray), gl.STATIC_DRAW);
-    var texCoordLoc = gl.getAttribLocation(program, "aTexCoord");
-    gl.vertexAttribPointer(texCoordLoc, 2, gl.FLOAT, false, 0, 0);
-    gl.enableVertexAttribArray(texCoordLoc);
+	// positions buffer
+	var vBuffer = gl.createBuffer();
+	gl.bindBuffer(gl.ARRAY_BUFFER, vBuffer);
+	gl.bufferData(gl.ARRAY_BUFFER, flatten(positionsArray), gl.STATIC_DRAW);
+	var positionLoc = gl.getAttribLocation(program, "aPosition");
+	gl.vertexAttribPointer(positionLoc, 4, gl.FLOAT, false, 0, 0);
+	gl.enableVertexAttribArray(positionLoc);
+
+	// texture buffer
+	var tBuffer = gl.createBuffer();
+	gl.bindBuffer(gl.ARRAY_BUFFER, tBuffer);
+	gl.bufferData(gl.ARRAY_BUFFER, flatten(texCoordsArray), gl.STATIC_DRAW);
+	var texCoordLoc = gl.getAttribLocation(program, "aTexCoord");
+	gl.vertexAttribPointer(texCoordLoc, 2, gl.FLOAT, false, 0, 0);
+	gl.enableVertexAttribArray(texCoordLoc);
 
 
+	var image = document.getElementById("texImage");
+	configureTexture(image);
 
-    thetaLoc = gl.getUniformLocation(program, "uTheta");
 
-    document.getElementById("ButtonX").onclick = function(){axis = xAxis;};
-    document.getElementById("ButtonY").onclick = function(){axis = yAxis;};
-    document.getElementById("ButtonZ").onclick = function(){axis = zAxis;};
-    document.getElementById("ButtonT").onclick = function(){flag = !flag;};
+	thetaLoc = gl.getUniformLocation(program, "uTheta");
 
-    render();
+	document.getElementById("ButtonX").onclick = function() {
+		axis = xAxis;
+	};
+	document.getElementById("ButtonY").onclick = function() {
+		axis = yAxis;
+	};
+	document.getElementById("ButtonZ").onclick = function() {
+		axis = zAxis;
+	};
+	document.getElementById("ButtonT").onclick = function() {
+		flag = !flag;
+	};
+
+	render();
 };
 
 function render() {
-    gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+	gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
 
-    if(flag) theta[axis] += 2.0;
-    gl.uniform3fv(thetaLoc, theta);
+	if (flag) theta[axis] += 2.0;
+	gl.uniform3fv(thetaLoc, theta);
 
-    gl.drawArrays( gl.TRIANGLES, 0, positionsArray.length );
-    requestAnimationFrame(render);
+	gl.drawArrays(gl.TRIANGLES, 0, positionsArray.length);
+	requestAnimationFrame(render);
 }
