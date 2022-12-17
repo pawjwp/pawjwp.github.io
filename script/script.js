e34bd38027b835af574279b11d3b8a2708fdd5ce
Pawjwp <pawjwp@users.noreply.github.com>
2022-12-16 21:48:03 -0500

    CR Progress

--- script/script.js
+++ script/script.js
@@ -2,3 +2,43 @@ function toggleSection(sectionID) {
 	document.querySelector(".active-content").classList.remove("active-content");
 	document.getElementById(sectionID).classList.add("active-content");
 }
+
+window.onhashchange = function(event) {
+	switchSection();
+}
+window.onload = function(event) {
+	switchSection();
+}
+
+function switchSection() {
+	switch(window.location.hash) {
+		case "#!home":
+			toggleSection('home');
+			break;
+		case '#!features':
+			toggleSection('features');
+			break;
+		case "#!quests":
+			toggleSection('quests');
+			break;
+		case "#!maps":
+			toggleSection('maps');
+			break;
+		case "#!items":
+			toggleSection('items');
+			break;
+		case "#!bags":
+			toggleSection('bags');
+			break;
+		case "#!tinkers":
+			toggleSection('tinkers');
+			break;
+		case "#!contact":
+			toggleSection('contact');
+			break;
+		default:
+			toggleSection('home');
+			break;
+	}
+    //window.location.hash = "#my-new-hash";
+};
